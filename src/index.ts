4b7aa1f9a298690bfbb415b0ac2291f0d39141c1
Dayblox <queiroz.raph@gmail.com>
2023-04-10 11:43:08 +0200

    ci

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log(process.env.PORT)
+function parse() {
+	const el = document.querySelector(".study__comments");
+	if (!el) return;
+	document.removeEventListener("DOMNodeInserted", parse);
+	el.addEventListener("DOMNodeInserted", () => {
+		const text = el.querySelector(".text")?.innerHTML;
+		if (!text?.length) return;
+		window.speechSynthesis.cancel();
+		const msg = new SpeechSynthesisUtterance(text);
+		msg.lang = "en";
+		msg.rate = 1.5;
+		msg.voice = window.speechSynthesis
+			.getVoices()
+			.find(({ name }) => name.includes("Google US English"));
+		window.speechSynthesis.speak(msg);
+	});
+}
+document.addEventListener("DOMNodeInserted", parse);
